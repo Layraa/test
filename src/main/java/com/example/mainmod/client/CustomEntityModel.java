2324fda4cbff01b333a9253ee20031526741dd89
Layraa <59958866+Layraa@users.noreply.github.com>
2025-04-17 13:19:11 +0300

    Add files via upload

---
+++ src/main/java/com/example/mainmod/client/CustomEntityModel.java
@@ -0,0 +1,24 @@
+package com.example.mainmod.client;
+
+import com.example.mainmod.MainMod;
+import com.example.mainmod.entity.CustomEntity;
+import net.minecraft.util.Identifier;
+import software.bernie.geckolib.model.GeoModel;
+
+public class CustomEntityModel extends GeoModel<CustomEntity> {
+
+    @Override
+    public Identifier getModelResource(CustomEntity entity) {
+        return MainMod.id("geo/" + entity.getModelPath());
+    }
+
+    @Override
+    public Identifier getTextureResource(CustomEntity entity) {
+        return MainMod.id("textures/entity/" + entity.getTexturePath());
+    }
+
+    @Override
+    public Identifier getAnimationResource(CustomEntity entity) {
+        return MainMod.id("animations/" + entity.getAnimationPath());
+    }
+}
---
+++ src/main/java/com/example/mainmod/client/CustomEntityRenderer.java
@@ -0,0 +1,20 @@
+package com.example.mainmod.client;
+
+import com.example.mainmod.entity.CustomEntity;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.util.Identifier;
+import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import com.example.mainmod.MainMod;
+
+public class CustomEntityRenderer extends GeoEntityRenderer<CustomEntity> {
+    public CustomEntityRenderer(EntityRendererFactory.Context context) {
+        super(context, new CustomEntityModel());
+    }
+
+    @Override
+    public Identifier getTextureLocation(CustomEntity entity) {
+        // Текстура больше не нужна, если мы получаем путь через GeoModel
+        // Но если хочешь, можешь оставить заглушку:
+        return MainMod.id("textures/entity/" + entity.getTexturePath());
+    }
+}
